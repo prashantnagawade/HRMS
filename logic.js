60fba37c4a111b04c569396ddd8510e46f578a29
Prashant Nagawade <prashantnagawade@gmail.com>
2018-03-30 21:45:19 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,10 @@
+'use strict';
+var NS = 'org.whiz.hrms';
+
+/**
+ * @param {org.whiz.hrms.applyForLeave} applyForLeave - apply for leaves
+ * @transaction
+ */
+function applyForLeave(applyForLeave) {
+	
+}
